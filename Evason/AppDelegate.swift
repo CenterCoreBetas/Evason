f3c8c216baf8933454db6595001725e19bf8b74e
Yashas Valmikam <yashas@Ravis-MacBook-Pro.local>
2018-12-21 08:28:08 -0800

    Initial Commit

---
+++ Evason/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Evason
+//
+//  Created by Yashas Valmikam on 12/21/18.
+//  Copyright © 2018 Yashas Valmikam. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
